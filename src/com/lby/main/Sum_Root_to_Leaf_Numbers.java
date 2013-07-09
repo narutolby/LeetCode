172c34c99b5e7cf68bf0f36f6cf86c15770badfc
liboyang <liboyang@meituan.com>
2013-07-09 10:18:41 +0800

    lby

--- src/com/lby/main/Sum_Root_to_Leaf_Numbers.java
+++ src/com/lby/main/Sum_Root_to_Leaf_Numbers.java
@@ -1,5 +1,27 @@
 package com.lby.main;
 
 public class Sum_Root_to_Leaf_Numbers {
+	int sum = 0;
+	public int sumNumbers(TreeNode root) {
+		sum=0;
+		if (root == null) {
+			return 0;
+		}
+		sum(root, 0);
+		return sum;
+	}
 
+	public void sum(TreeNode root, int index) {
+      if(root==null){
+    	  return;
+      }
+      index*=10;
+      index+=root.val;
+      if(root.left==null && root.right==null){
+    	  sum+=index;
+    	  return;
+      }
+      sum(root.left,index);
+      sum(root.right,index);
+	}
 }
