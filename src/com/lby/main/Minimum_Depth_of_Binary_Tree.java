3e3ee2147c5deaa906c777e45a9619999dbf29ac
liboyang <liboyang@meituan.com>
2013-07-11 21:49:11 +0800

    ddd

---
+++ src/com/lby/main/Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,21 @@
+package com.lby.main;
+
+public class Minimum_Depth_of_Binary_Tree {
+	public int minDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		if(root.left==null && root.right==null){
+			return 1;
+		}
+		int leftDepth = minDepth(root.left);
+		int rightDepth = minDepth(root.right);
+		if(leftDepth==0){
+			leftDepth = Integer.MAX_VALUE;
+		}
+		if(rightDepth == 0){
+			rightDepth = Integer.MAX_VALUE;
+		}
+		return 1+Math.min(leftDepth,rightDepth );
+	}
+}
