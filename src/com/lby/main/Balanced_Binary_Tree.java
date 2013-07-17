085bf23a84eec41057bb94f017f5f6b05028acfc
liboyang <liboyang@meituan.com>
2013-07-17 23:43:39 +0800

    dd

--- src/com/lby/main/Balanced_Binary_Tree.java
+++ src/com/lby/main/Balanced_Binary_Tree.java
@@ -1,5 +1,32 @@
 package com.lby.main;
 
 public class Balanced_Binary_Tree {
+	public static boolean isBalanced(TreeNode root) {
+		int depth = depthOfTree(root);
+		if (depth == -1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public static int depthOfTree(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		if (root.left == null && root.right == null) {
+			return 1;
+		}
+		int leftPath = depthOfTree(root.left);
+		if (leftPath == -1) {
+			return -1;
+		}
+		int rightPath = depthOfTree(root.right);
+		if (rightPath == -1 || Math.abs(rightPath - leftPath) > 1) {
+			return -1;
+		} else {
+			return Math.max(leftPath, rightPath) + 1;
+		}
+	}
 
 }
