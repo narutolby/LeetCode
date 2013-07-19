9cec8955ac00bc1da922f825ec04717d609169de
liboyang <liboyang@meituan.com>
2013-07-19 23:50:27 +0800

    d

--- src/com/lby/main/Balanced_Binary_Tree.java
+++ src/com/lby/main/Balanced_Binary_Tree.java
@@ -1,8 +1,12 @@
 package com.lby.main;
 
+/*
+ *Given a binary tree, determine if it is height-balanced.
+ *For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ */
 public class Balanced_Binary_Tree {
 	public static boolean isBalanced(TreeNode root) {
-		int depth = depthOfTree(root);
+		int depth = depthOfTree(root, null);
 		if (depth == -1) {
 			return false;
 		} else {
@@ -10,18 +14,21 @@ public class Balanced_Binary_Tree {
 		}
 	}
 
-	public static int depthOfTree(TreeNode root) {
+	public static int depthOfTree(TreeNode root, Integer depth) {
 		if (root == null) {
 			return 0;
 		}
 		if (root.left == null && root.right == null) {
 			return 1;
 		}
-		int leftPath = depthOfTree(root.left);
+		int leftPath = depthOfTree(root.left, null);
 		if (leftPath == -1) {
 			return -1;
 		}
-		int rightPath = depthOfTree(root.right);
+		if (depth != null && leftPath - depth > 1) {
+			return -1;
+		}
+		int rightPath = depthOfTree(root.right, leftPath);
 		if (rightPath == -1 || Math.abs(rightPath - leftPath) > 1) {
 			return -1;
 		} else {
---
+++ src/com/lby/main/Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,22 @@
+package com.lby.main;
+
+/*
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+ */
+public class Binary_Tree_Inorder_Traversal {
+
+}
