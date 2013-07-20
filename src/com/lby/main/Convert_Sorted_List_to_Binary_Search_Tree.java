a75a98f79f208cddf180be02fa01085c5181200d
liboyang <liboyang@meituan.com>
2013-07-20 21:36:14 +0800

    d

--- src/com/lby/main/Convert_Sorted_List_to_Binary_Search_Tree.java
+++ src/com/lby/main/Convert_Sorted_List_to_Binary_Search_Tree.java
@@ -13,18 +13,26 @@ import java.util.List;
 public class Convert_Sorted_List_to_Binary_Search_Tree {
 
 	public static TreeNode sortedListToBST(ListNode head) {
-		if(head==null){
+		if (head == null) {
 			return null;
 		}
-	   List<ListNode> list = new ArrayList<ListNode>();
-		while(head.next!=null){
+		List<ListNode> list = new ArrayList<ListNode>();
+		while (head != null) {
 			list.add(head);
+			head = head.next;
 		}
-		return sortedListToBST1(list);
+		int l = 0, r = list.size() - 1;
+		return sortedListToBST(list, l, r);
 	}
 
-	public static TreeNode sortedListToBST1(List<TreeNode> list) {
-          		
+	public static TreeNode sortedListToBST(List<ListNode> list, int l, int r) {
+		if (l <= r) {
+			int m = (l + r) / 2;
+			TreeNode root = new TreeNode(list.get(m).val);
+			root.left = sortedListToBST(list, l, m - 1);
+			root.right = sortedListToBST(list, m + 1, r);
+			return root;
+		}
 		return null;
 	}
 }
