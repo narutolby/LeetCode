c2ebcabc2c4e354f53bc6e16740d664815859417
liboyang <liboyang@meituan.com>
2013-07-23 00:01:51 +0800

    dd

--- src/com/lby/main/Same_Tree.java
+++ src/com/lby/main/Same_Tree.java
@@ -1,23 +1,26 @@
 package com.lby.main;
+
 /**
  * Given two binary trees, write a function to check if they are equal or not.
- * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+ * Two binary trees are considered equal if they are structurally identical and
+ * the nodes have the same value.
+ * 
  * @author narutolby
- *
+ * 
  */
 public class Same_Tree {
-	 public boolean isSameTree(TreeNode p, TreeNode q) {
-		    if(p==null && q== null){
-		    	return true;
-		    }else if(p!=null && q!=null){
-                  if(p.val!=q.val){
-                	  return false;
-                  }else{
-                	  boolean is = isSameTree(p.left,q.left);
-                	  return is?isSameTree(p.right,q.right):false;
-                  }
-		    }else{
-		    	return false;
-		    }
-	 }
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		} else if (p != null && q != null) {
+			if (p.val != q.val) {
+				return false;
+			} else {
+				boolean is = isSameTree(p.left, q.left);
+				return is ? isSameTree(p.right, q.right) : false;
+			}
+		} else {
+			return false;
+		}
+	}
 }
--- src/com/lby/main/Sort_Colors.java
+++ src/com/lby/main/Sort_Colors.java
@@ -1,5 +1,45 @@
 package com.lby.main;
 
+/**
+ * Given an array with n objects colored red, white or blue, sort them so that
+ * objects of the same color are adjacent, with the colors in the order red,
+ * white and blue.
+ * 
+ * Here, we will use the integers 0, 1, and 2 to represent the color red, white,
+ * and blue respectively.
+ * 
+ * Note: You are not suppose to use the library's sort function for this
+ * problem.
+ * 
+ * Follow up: A rather straight forward solution is a two-pass algorithm using
+ * counting sort. First, iterate the array counting number of 0's, 1's, and 2's,
+ * then overwrite array with total number of 0's, then 1's and followed by 2's.
+ * 
+ * Could you come up with an one-pass algorithm using only constant space?
+ * 
+ * @author narutolby
+ * 
+ */
 public class Sort_Colors {
+	public void swap(int[] A, int i, int j) {
+		int tmp = A[i];
+		A[i] = A[j];
+		A[j] = tmp;
+	}
 
+	public void sortColors(int[] A) {
+		int r = 0, w = 0, b = A.length - 1;
+		while (w <= b) {
+            if(A[w]==0){
+            	swap(A,r,w);
+            	r++;
+            	w++; 
+            }else if(A[w]==2){
+            	swap(A,w,b);
+            	b--;
+            }else{
+            	w++;
+            }
+		}
+	}
 }
