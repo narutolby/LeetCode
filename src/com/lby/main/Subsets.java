868c04d6d3881f4a1035e48254230fd55ce6d5bc
narutolby <willianlby@yahoo.cn>
2013-07-26 01:07:03 +0800

    Given a set of distinct integers, S, return all possible subsets.

---
+++ src/com/lby/main/Subsets.java
@@ -0,0 +1,49 @@
+package com.lby.main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+ * Given a set of distinct integers, S, return all possible subsets.
+
+Note:
+
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+ */
+public class Subsets {
+	 public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+             ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+             ArrayList<Integer> sub = new ArrayList<Integer>();
+             list.add(sub);
+             for(int i : S){
+            	 int len = list.size();
+            	 for(int j=0;j<len;j++){
+            		 sub= new ArrayList<Integer>();
+            		 sub.addAll(list.get(j)) ;
+            		 sub.add(S[i]);
+            		 list.add(sub);
+            	 }
+             }
+             return list;
+	    }
+	 public static void main(String[]args){
+		 int[] array = {1,2,3};
+		 for(int i:array){
+			 System.out.println(i);
+		 }
+	 }
+}
