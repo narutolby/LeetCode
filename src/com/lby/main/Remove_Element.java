c1856ccf3e75623e8e7aa9aa3b1fada8e9183514
narutolby <willianlby@yahoo.cn>
2013-08-01 01:12:58 +0800

    Given an array and a value, remove all instances of that value in place and return the new length.

--- src/com/lby/main/Remove_Element.java
+++ src/com/lby/main/Remove_Element.java
@@ -1,5 +1,22 @@
 package com.lby.main;
 
-public class Remove_Element {
+import java.util.Arrays;
 
+/*
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ */
+public class Remove_Element {
+	public int removeElement(int[] A, int elem) {
+        int s=0,x=0,len = A.length; 
+        while(x<len){
+        	if(A[x]!=elem){
+        		A[s] = A[x];
+        		s++;
+        	}
+        	x++;
+        }
+        return s;
+    }
 }
