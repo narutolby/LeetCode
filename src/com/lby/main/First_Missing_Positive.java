20086d9900a1cfaa69ee83928fa76075605f64a8
narutolby <willianlby@yahoo.cn>
2013-08-06 00:51:02 +0800

    Given an unsorted integer array, find the first missing positive integer.

---
+++ src/com/lby/main/First_Missing_Positive.java
@@ -0,0 +1,38 @@
+package com.lby.main;
+/*
+ * Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+ */
+public class First_Missing_Positive {
+    public int firstMissingPositive(int[] A) {
+        int len = A.length;
+        if(len==0){
+            return 1;
+        }
+        int i = 0;
+        while(i<len){
+            if(A[i]>=1 && A[i]<=len && A[i]!=i+1 && A[A[i]-1]!=A[i]) {
+                int tmp = A[i],tmp1 = A[tmp-1];
+                A[i] =  tmp1;
+                A[tmp-1] = tmp;
+                continue;
+            }
+            i++;
+        }
+        i=0;
+        while(i<len && A[i]==i+1){
+            i++;
+        }
+        return i+1;
+
+    }
+    public static void main(String[]args){
+        int[]a = {0};
+        System.out.println(new First_Missing_Positive().firstMissingPositive(a));
+    }
+}
