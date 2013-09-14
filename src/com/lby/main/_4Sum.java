be8582551a1d7bef6cfa6ca4a03857d274e88156
narutolby <willianlby@yahoo.cn>
2013-09-15 01:19:38 +0800

    Add another method for this problem.Passed!

--- src/com/lby/main/_4Sum.java
+++ src/com/lby/main/_4Sum.java
@@ -21,6 +21,10 @@ import java.util.Set;
  * (-2,  0, 0, 2)
  */
 public class _4Sum {
+    /*
+     * 小数据测试通过，大数据不过
+     */
+
     Set<String> set = new HashSet<String>();
     public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
         ArrayList<ArrayList<Integer>> root  = new ArrayList<ArrayList<Integer>>();
@@ -57,7 +61,53 @@ public class _4Sum {
 
     }
     public static void main(String[]args){
-        int[]num = {0,0,0,0};
-        System.out.println(new _4Sum().fourSum(num, 0));
+        int[]num = {-1,2,2,-5,0,-1,4};
+        System.out.println(new _4Sum()._fourSum(num, 3));
+    }
+    /*
+     * 小数据测试通过，大数据测试通过，oye
+     */
+
+    public ArrayList<ArrayList<Integer>> _fourSum(int[] num, int target) {
+        ArrayList<ArrayList<Integer>> rootList = new ArrayList<ArrayList<Integer>>();
+        Set<String> set = new HashSet<String>();
+        StringBuffer sb = new StringBuffer();
+        int len = num.length;
+        if(len < 4){
+            return rootList;
+        }
+        Arrays.sort(num);
+        for(int i=0;i<len-3;i++){
+            for(int j=i+3;j<len;j++){
+                int t = target - num[i] - num[j];
+                int l = i+1,r = j-1;
+                while(l<r){
+                    if(num[l] + num[r] < t){
+                        l++;
+                    }else if(num[l] + num[r] > t){
+                        r--;
+                    }else{
+                        sb.setLength(0);
+                        ArrayList<Integer> list = new ArrayList<Integer>();
+                        list.add(num[i]);
+                        list.add(num[l]);
+                        list.add(num[r]);
+                        list.add(num[j]);
+                        sb.append(num[i]);
+                        sb.append(num[l]);
+                        sb.append(num[r]);
+                        sb.append(num[j]);
+                        String str = sb.toString();
+                        if(!set.contains(str)){
+                            set.add(str);
+                            rootList.add(list);
+                        }
+                        l++;
+                        r--;
+                    }
+                }
+            }
+        }
+        return rootList;
     }
 }
